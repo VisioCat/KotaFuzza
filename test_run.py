8d539b497ae55cee0788bc3dffea9386558fccc6
fablocke-Telekom <59057282+fablocke-Telekom@users.noreply.github.com>
2020-01-06 20:32:09 +0100

    Create test_run.py

---
+++ test_run.py
@@ -0,0 +1,18 @@
+from KotaFuzza import Fuzz_Preperation, Fuzz_with_Seeds, Fuzz_create_Seeds, Fuzz_Simple
+
+TARGETIP = '192.168.0.1'
+TARGETPORT = 443
+TARGETTRANSPORT = 'udp'
+TLSon = True
+PCAP_FILE = '../trace1.pcapng'
+RAW_DIR = '../raw_packets/'
+SEED_FILE = '../seeds.csv
+
+#x = Fuzz_Preperation(PCAP_FILE, RAW_DIR, TARGETIP, TLSOFFSET, TLSon)
+#x.parse()
+
+#firstfuzz = Fuzz_create_Seeds(TARGETIP, TARGETPORT, TARGETTRANSPORT, RAW_DIR, SEED_FILE)
+#firstfuzz.start()
+
+#secondfuzz = Fuzz_with_Seeds(TARGETIP, TARGETPORT, TARGETTRANSPORT, RAW_DIR, SEED_FILE)
+#secondfuzz.start()
